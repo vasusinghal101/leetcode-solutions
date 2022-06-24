3a12086c8fa7caa17e360ad1674d6a0344f0ac4c
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-06-24 18:22:50 +0530

    Time: 26 ms (5.05%), Space: 56.9 MB (83.84%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,66 @@
+// class Solution {
+//     public boolean isPossible(int[] target) {
+//         if(target.length==1){
+//             return target[0]==1;
+//         }
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (b-a));
+//         int totalSum=0;
+//         for(int n:target){
+//             pq.add(n);
+//             totalSum+=n;
+//         }
+        
+//         while(true){
+//             int largest = pq.remove();
+//             int restSum = totalSum - largest;
+//             if(largest==1 || restSum==1){
+//                 return true;
+//             }
+//             if(largest<=restSum || largest%restSum==0){
+//                  return false;
+//             }else{
+//                 int newNum = largest%restSum;
+//                 totalSum = newNum+restSum;
+//                 pq.add(newNum);
+//             }
+//         }
+//     }
+// }
+
+class Solution {
+    public boolean isPossible(int[] target) {
+        
+        // Handle the n = 1 case.
+        if (target.length == 1) {
+            return target[0] == 1;
+        }
+        
+        int totalSum = Arrays.stream(target).sum();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for (int num : target) {
+            pq.add(num);
+        }
+        
+        while (pq.element() > 1) {
+            int largest = pq.remove();
+            int rest = totalSum - largest;
+            
+            // This will only occur if n = 2.
+            if (rest == 1) {
+                return true;
+            }
+            int x = largest % rest;
+            
+            // If x is now 0 (invalid) or didn't
+            // change, then we know this is impossible.
+            if (x == 0 || x == largest) {
+                return false;
+            }
+            pq.add(x);
+            totalSum = totalSum - largest + x;
+        }
+        
+        return true; 
+    }
+}
