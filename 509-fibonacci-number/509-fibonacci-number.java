572081aee77c82bbe36f39521b6f2fae9bfc7229
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-07-06 21:46:46 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (17.51%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int temp = a+b;
+            a=b;
+            b=temp;
+        }
+        
+        return b;
+    }
+}
