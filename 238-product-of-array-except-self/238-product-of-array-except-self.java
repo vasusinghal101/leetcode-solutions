81e1311c26cbb1ed6645ea4400620021e7d5e623
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-07-19 22:25:55 +0530

    Time: 3 ms (51.58%), Space: 57.2 MB (79.86%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,26 +1,70 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        boolean z = false;
+//         boolean z = false;
         
-        int p = 1;
-        for(int n :nums){
-            if(n==0){
-                if(!z){
-                    z=true;
-                    continue;
-                }
-            }
-            p*=n;
+//         int p = 1;
+//         for(int n :nums){
+//             if(n==0){
+//                 if(!z){
+//                     z=true;
+//                     continue;
+//                 }
+//             }
+//             p*=n;
+//         }
+        
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 nums[i]= !z ? p/nums[i] : 0;
+//             }else{
+//                 nums[i] = p;
+//             }
+//         }
+        
+//         return nums;
+        
+               // The length of the input array
+        int length = nums.length;
+
+        // The left and right arrays as described in the algorithm
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // L[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so L[0] would be 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+
+            // L[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            L[i] = nums[i - 1] * L[i - 1];
         }
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[i]= !z ? p/nums[i] : 0;
-            }else{
-                nums[i] = p;
-            }
+
+        // R[i] contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R[length - 1] would be 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+
+            // R[i + 1] already contains the product of elements to the right of 'i + 1'
+            // Simply multiplying it with nums[i + 1] would give the product of all
+            // elements to the right of index 'i'
+            R[i] = nums[i + 1] * R[i + 1];
         }
-        
-        return nums;
+
+        // Constructing the answer array
+        for (int i = 0; i < length; i++) {
+            // For the first element, R[i] would be product except self
+            // For the last element of the array, product except self would be L[i]
+            // Else, multiple product of all elements to the left and to the right
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
     }
 }
