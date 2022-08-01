1b1a1169e03fe09bf150e54b5dd618d61957dfdd
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-01 23:10:22 +0530

    Time: 1 ms (94.90%), Space: 42.6 MB (57.79%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,56 +14,40 @@
  * }
  */
 class Solution {
-//     public List<Integer> rightSideView(TreeNode root) {
-//         if (root == null)
-//             return new ArrayList();
+    public void dfs(TreeNode root, int level, List<Integer> ans) {
+         if(root == null)return;
         
-//         List<Integer> ans = new ArrayList<Integer>();
+         if(level>ans.size())ans.add(root.val);
+
+         dfs(root.right, level+1, ans);
+         dfs(root.left, level+1, ans);
+
+    }
+public List<Integer> rightSideView(TreeNode root) {
+        if (root == null)
+            return new ArrayList();
         
+        List<Integer> ans = new ArrayList<Integer>();
+        dfs(root,1,ans);
+        return ans;
+    
+        //bfs
 //         LinkedList<TreeNode> q = new LinkedList<>();
         
 //         q.addLast(root);
         
 //         while(q.size()>0){
 //             int s = q.size();
-//             boolean flag=false;
 //             while(s-- > 0){
 //                TreeNode r = q.removeFirst();
-//                 if(!flag){
+//                 if(s == 0)
 //                     ans.add(r.val);
-//                     flag=true;
-//                 }
+//                 if(r.left!=null)
+//                  q.addLast(r.left);
 //                 if(r.right!=null)
 //                  q.addLast(r.right);
-//                  if(r.left!=null)
-//                  q.addLast(r.left);
 //             }            
-            
 //         }
-        
 //         return ans;
-//     }
-public List<Integer> rightSideView(TreeNode root) {
-        if (root == null)
-            return new ArrayList();
-        
-        List<Integer> ans = new ArrayList<Integer>();
-        LinkedList<TreeNode> q = new LinkedList<>();
-        
-        q.addLast(root);
-        
-        while(q.size()>0){
-            int s = q.size();
-            while(s-- > 0){
-               TreeNode r = q.removeFirst();
-                if(s == 0)
-                    ans.add(r.val);
-                if(r.left!=null)
-                 q.addLast(r.left);
-                if(r.right!=null)
-                 q.addLast(r.right);
-            }            
-        }
-        return ans;
     }
 }
