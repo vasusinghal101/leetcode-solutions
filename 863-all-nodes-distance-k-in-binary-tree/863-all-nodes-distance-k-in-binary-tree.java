c4493ad2fac38713b796a9d8983bde7430fad7fc
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-09 00:16:57 +0530

    Time: 13 ms (83.70%), Space: 42.9 MB (64.25%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public boolean path(TreeNode root, TreeNode target, List<TreeNode> r2n){
+        if(root==null) return false;
+        
+        if(root == target){
+            r2n.add(root);
+            return true;
+        }
+        
+        r2n.add(root);
+        if(path(root.left,target,r2n)) return true;
+        if(path(root.right,target,r2n)) return true;
+        
+        r2n.remove(r2n.size()-1);
+        return false;
+    }
+    
+    public void kLevelDown(TreeNode root, TreeNode blocker, int k, List<Integer> ans){
+        if(root == null || root==blocker || k<0) return;
+        
+        if(k==0){
+            ans.add(root.val);
+            return;
+        }
+        kLevelDown(root.left,blocker,k-1,ans);
+        kLevelDown(root.right,blocker,k-1,ans);
+        
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<TreeNode> r2n = new ArrayList<TreeNode>();
+        List<Integer> ans = new ArrayList<>();
+        if(!path(root,target,r2n)) return ans;
+        
+        r2n.add(null);
+        
+        for(int i = r2n.size()-2;i>=0;i--){
+            TreeNode blocker = r2n.get(i+1);
+            TreeNode start = r2n.get(i);
+            kLevelDown(start, blocker, k, ans);
+            k--;
+        }
+        return ans;
+    }
+}
