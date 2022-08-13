2734f089dbe3bc392cf5b0abcdd632b2c4617041
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-13 23:32:32 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (33.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -2,9 +2,9 @@ class Solution {
     public int climbStairs(int n) {
         int a=1;
         int b=1;
-        
+        int sum=0;
         for(int i=2;i<=n;i++){
-            int sum = a+b;
+            sum = a+b;
             a=b;
             b=sum;
         }
