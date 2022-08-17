2a38bc08f049e0156737600017525aeb6a6156ce
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-18 02:30:07 +0530

    Time: 2 ms (83.17%), Space: 43.6 MB (94.65%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,14 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l = 0;
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[l]=nums[i];
-                l++;
+//         int l = 0;
+//         for(int i = 0; i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 nums[l]=nums[i];
+//                 l++;
+//             }
+//         }
+        
+//         for(int i = l;i<nums.length;i++)
+//             nums[i]=0;
+         int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
             }
         }
-        
-        for(int i = l;i<nums.length;i++)
-            nums[i]=0;
     }
 }
