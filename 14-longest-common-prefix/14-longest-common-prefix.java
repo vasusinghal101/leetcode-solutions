33cd0ef6a54a27a4eeed226bf980904ee5dc2097
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-31 00:57:11 +0530

    Time: 1 ms (89.09%), Space: 42.5 MB (30.45%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,27 +1,38 @@
 class Solution {
+//    public String longestCommonPrefix(String[] strs) {
+//         StringBuilder sb = new StringBuilder();
+//         int j = 0;
+//         char ch = '\0';
+//         boolean flag=true;
+//         while(flag){
+//            if(j >= strs[0].length()){
+//                flag = false;     
+//                break;
+//            }else{
+//                ch = strs[0].charAt(j);
+//            }
+//            for(int i=1; i<strs.length;i++){
+//                 if(j >= strs[i].length() || ch != strs[i].charAt(j)){
+//                     flag =false;
+//                     break;
+//                 }
+//             }
+//             if(flag) sb.append(ch);
+//             j++;
+//         }
+        
+//         return sb.toString();
+        
+//     }
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
-        int j = 0;
-        char ch = '\0';
-        boolean flag=true;
-        while(flag){
-           if(j >= strs[0].length()){
-               flag = false;     
-               break;
-           }else{
-               ch = strs[0].charAt(j);
-           }
-           for(int i=1; i<strs.length;i++){
-                if(j >= strs[i].length() || ch != strs[i].charAt(j)){
-                    flag =false;
-                    break;
-                }
-            }
-            if(flag) sb.append(ch);
-            j++;
+    if (strs == null || strs.length == 0) return "";
+    for (int i = 0; i < strs[0].length() ; i++){
+        char c = strs[0].charAt(i);
+        for (int j = 1; j < strs.length; j ++) {
+            if (i == strs[j].length() || strs[j].charAt(i) != c)
+                return strs[0].substring(0, i);             
         }
-        
-        return sb.toString();
-        
     }
+    return strs[0];
+}
 }
