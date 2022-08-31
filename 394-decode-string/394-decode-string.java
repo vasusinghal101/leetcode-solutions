b473234a62b6f8d92fe5bddd87d45472c13135cf
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-08-31 15:13:42 +0530

    Time: 8 ms (22.61%), Space: 42.4 MB (28.91%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i<s.length()){
+            char ch = s.charAt(i);
+            if(ch!=']'){
+                st.push(ch);
+            }else{
+                String str="";
+                while(st.peek()!='['){
+                    str+=st.pop();
+                }
+                st.pop();
+                String digit="";
+                while(st.size()>0 && st.peek() >='0' && st.peek()<='9'){
+                    digit += st.pop();
+                }
+                
+                str = reverse(str);
+                System.out.println(str);
+                int d = Integer.parseInt(reverse(digit));
+                for(int k = 0; k<d;k++){
+                    for(char c: str.toCharArray()){
+                        st.push(c);
+                    }
+                }
+            }
+            i++;
+        }
+        
+        while(st.size()>0){
+            sb.append(st.pop());
+        }
+        sb = sb.reverse();
+        return sb.toString();
+        
+    }
+    
+    public String reverse(String s) {
+        char[] arr = s.toCharArray();
+        int i =0;
+        int j = s.length()-1;
+        while(i<j){
+            char t = arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+            i++;
+            j--;
+        }
+        
+        return String.valueOf(arr);
+    }
+}
