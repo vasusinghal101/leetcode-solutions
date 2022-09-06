b3f202477ab954acc4e0c714d0562e9c7e076d6e
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-09-07 02:31:48 +0530

    Time: 168 ms (54.22%), Space: 60.8 MB (87.12%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,6 +1,9 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        HashMap<String, Integer> hm = new HashMap(); 
+        //Can  be done in single pass keep count of symmetric 
+        //update it accordingly in last check if>0
+        HashMap<String, Integer> hm = new HashMap();
+        int sym = 0;
         int ans = 0;
         for(String word: words){
             char ch1 = word.charAt(0);
@@ -9,17 +12,20 @@ class Solution {
             if(hm.getOrDefault(reverse, 0)>0){
                 ans+=4;
                 hm.put(reverse, hm.get(reverse)-1);
+                sym += ch1==ch2 ? -1 : 0;
             }else{
                 hm.put(word, hm.getOrDefault(word,0)+1);
+                 sym += ch1==ch2 ? 1 : 0;
             } 
         }
         
-        for(String word: hm.keySet()){
-            if(word.charAt(0) == word.charAt(1) && hm.get(word)>0){
-              ans+=2;
-              break;
-            }
-        }
-        return ans;
+        // for(String word: hm.keySet()){
+        //     if(word.charAt(0) == word.charAt(1) && hm.get(word)>0){
+        //       ans+=2;
+        //       break;
+        //     }
+        // }
+        
+        return ans + (sym>0 ? 2 : 0);
     }
 }
