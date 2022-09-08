7886db407465a8a5a1960319b1b956dd4986d21d
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-09-08 23:15:03 +0530

    Time: 2 ms (49.41%), Space: 42.2 MB (51.16%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,16 +14,48 @@
  * }
  */
 class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root==null){
+//             return new ArrayList<Integer>();
+//         }
+        
+//         List<Integer> l = inorderTraversal(root.left);
+//         l.add(root.val);
+//         List<Integer> r = inorderTraversal(root.right);
+        
+//            l.addAll(r);
+//            return l;
+//     }
+    class Pair{
+        TreeNode node;
+        int state;
+        
+        Pair(TreeNode node, int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root==null){
-            return new ArrayList<Integer>();
+        List<Integer> ans = new ArrayList<Integer>();
+        
+        if(root==null) return ans;
+        
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(root, 0));
+        
+        while(st.size()>0){
+            Pair p = st.peek();
+            if(p.state==0){//preoOrder
+                if(p.node.left!= null) st.push(new Pair(p.node.left, 0));
+                p.state++;
+            }else if(p.state == 1){//inorder
+                ans.add(p.node.val);
+                if(p.node.right != null) st.push(new Pair(p.node.right, 0));
+                p.state++;
+            }else{//postorder
+                st.pop();
+            }
         }
         
-        List<Integer> l = inorderTraversal(root.left);
-        l.add(root.val);
-        List<Integer> r = inorderTraversal(root.right);
-        
-           l.addAll(r);
-           return l;
-    }
-}
+        return ans;
+}}
