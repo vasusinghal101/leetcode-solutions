2cb0f4dd353fd66f708f9c7c04b5d661b0253059
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-09-09 02:21:49 +0530

    Time: 43 ms (14.97%), Space: 43.8 MB (67.23%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -16,39 +16,39 @@
 
 //for optimization first check count of sub array sum equal to k 560.
 class Solution {
-     public int pathSum(TreeNode root, int target) {
-        if(root==null) return 0;
-         HashMap<Long, Integer> hm = new HashMap<>();
-         hm.put(0l,1);
-         return helper(root, target, 0, hm);
-    }
-    public int helper(TreeNode root, long target, long ssf, HashMap<Long, Integer> prefixSums){
-        if(root == null) return 0;
-        long s = ssf+root.val;
-        int ans = 0;
-        ans+= prefixSums.getOrDefault((s - target),0);// count ending at me 
-        //if(s==18) System.out.print(prefixSums + ""+ans);
-        prefixSums.put(s,prefixSums.getOrDefault(s,0)+1);
-        ans+= helper(root.left, target, s, prefixSums);
-         ans+= helper(root.right, target, s, prefixSums);
-        prefixSums.put(s,prefixSums.getOrDefault(s,0)-1);
-        return ans;
+//      public int pathSum(TreeNode root, int target) {
+//         if(root==null) return 0;
+//          HashMap<Long, Integer> hm = new HashMap<>();
+//          hm.put(0l,1);
+//          return helper(root, target, 0, hm);
+//     }
+//     public int helper(TreeNode root, long target, long ssf, HashMap<Long, Integer> prefixSums){
+//         if(root == null) return 0;
+//         long s = ssf+root.val;
+//         int ans = 0;
+//         ans+= prefixSums.getOrDefault((s - target),0);// count ending at me 
+//         //if(s==18) System.out.print(prefixSums + ""+ans);
+//         prefixSums.put(s,prefixSums.getOrDefault(s,0)+1);
+//         ans+= helper(root.left, target, s, prefixSums);
+//          ans+= helper(root.right, target, s, prefixSums);
+//         prefixSums.put(s,prefixSums.getOrDefault(s,0)-1);
+//         return ans;
         
+//     }
+    public int helper(TreeNode root, long target){//including me
+        if(root==null) return 0;
+        int ans=0;
+        if(target==root.val) ans++;
+        ans+= helper(root.left, target - root.val);
+        ans+= helper(root.right, target - root.val);
+        return ans;
     }
-    // public int helper(TreeNode root, long target){
-    //     if(root==null) return 0;
-    //     int ans=0;
-    //     if(target==root.val) ans++;
-    //     ans+= helper(root.left, target - root.val);
-    //     ans+= helper(root.right, target - root.val);
-    //     return ans;
-    // }
-    // public int pathSum(TreeNode root, int target) {
-    //     if(root==null) return 0;
-    //     int ans=0;
-    //      ans+= helper(root, target);
-    //      ans+= pathSum(root.left, target);
-    //      ans+= pathSum(root.right, target);
-    //     return ans;
-    // }
+    public int pathSum(TreeNode root, int target) {
+        if(root==null) return 0;
+        int ans=0;
+         ans+= helper(root, target);
+         ans+= pathSum(root.left, target);
+         ans+= pathSum(root.right, target);
+        return ans;
+    }
 }
