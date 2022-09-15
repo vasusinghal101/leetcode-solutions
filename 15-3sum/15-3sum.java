b740d5a8fb7908c6da287f8a98f9ab7a23fa0be4
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-09-15 22:54:10 +0530

    Time: 36 ms (52.48%), Space: 61.9 MB (25.78%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,6 +3,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
+            if(nums[i]>0) break;
             if(i==0 || nums[i]!=nums[i-1]){
             ArrayList<int[]> indexes = twoSum(nums, -1*nums[i], i+1);
             if(indexes.size()>0){
