99eaeac68c5ed249d8543c2ed8d7976f3ab02d64
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2022-11-04 23:36:34 +0530

    Time: 6 ms (74.24%), Space: 4 MB (58.33%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
@@ -0,0 +1,25 @@
+func reverseVowels(s string) string {
+    
+    i:= 0
+    j:=len(s)-1
+    runes := []byte(s)
+    for i<j {
+        for i<=j && !isVowel(runes[i]){
+            i++
+        }
+        for !isVowel(runes[j]) && j>=i {
+            j--
+        }
+        if i<j {
+            runes[i], runes[j] = runes[j], runes[i]
+            i++
+            j--
+        }
+    }
+    return string(runes)
+}
+
+func isVowel(c byte) bool {
+    if c < 'a' { c += 32 }
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+}
