f3e8de92a67d18dc9d21aae7f242d07d7ab57e0d
Vasu Singhal <54227750+vasusinghal101@users.noreply.github.com>
2023-06-04 23:24:28 +0530

    Time: 1 ms (98.97%), Space: 44.1 MB (22.49%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,30 @@
+class Solution {
+    //To do with DSU
+    public int findCircleNum(int[][] isConnected) {
+        int count = 0;
+        boolean[] visited = new boolean[isConnected.length];
+        
+        for(int i =0;i<visited.length;i++){
+            if(!visited[i]){
+                count++;
+                gcc(isConnected,i,visited);
+            }
+        }
+        
+        return count;
+    }
+    
+    public void gcc(int[][] graph, int src, boolean[] visited){
+        if(visited[src]){
+            return;
+        }
+        
+        visited[src] = true;
+        
+        for(int i=0;i<graph[0].length;i++){
+            if(graph[src][i]==1){
+                gcc(graph,i,visited);
+            }
+        }
+    }
+}
